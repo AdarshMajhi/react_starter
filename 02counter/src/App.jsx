1904ed0471a98919742a2d23a4d85907024d6407
Rahul kumar <adarshakumarmajhi@accuratics.com>
2024-12-29 11:18:32 +0530

    Modified_Counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,25 +4,29 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-
   let [counter, setCounter] = useState(0);
 
   const addValue = () => {
-    setCounter(counter + 1);
-  }
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
 
   const subtractValue = () => {
-    setCounter(counter - 1);
-  }
- 
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <>
       <h1>Welcome To Counter</h1>
       <h2>Count : {counter}</h2>
 
-      <button onClick={addValue}>Increase</button> 
+      <button onClick={addValue}>Increase</button>
       <br />
       <button onClick={subtractValue}>Decrease</button>
+      <p>Note : Count can't go below 0 and 20 is the max counting</p>
     </>
   );
 }
